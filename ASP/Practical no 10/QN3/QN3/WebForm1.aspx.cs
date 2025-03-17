9e232b2017e012f71d6d19cc00cb51e32a0a1420
PratikSV <pratik.dg@somaiya.edu>
2025-03-17 22:21:03 +0530

    done with qn3 in prac 10 in ASP

--- ASP/Practical no 10/QN3/QN3/WebForm1.aspx.cs
+++ ASP/Practical no 10/QN3/QN3/WebForm1.aspx.cs
@@ -11,6 +11,8 @@ namespace QN3
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
+        
+        
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -18,34 +20,43 @@ namespace QN3
 
         protected void Registerbtn(object sender, EventArgs e)
         {
-            string Fname = TextBox1.Text;
-            string Lname = TextBox2.Text;
-            int age = int.Parse(TextBox3.Text);
-            string accounttype = DropDownList1.SelectedValue;
-            string address = TextBox6.Text;
-            string email = TextBox4.Text;
-            int mobile = Int32.Parse(TextBox5.Text);
-
-            String connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"B:\\Collage BCA\\Sybca\\Sem 4\\SYBCA-Practicals\\ASP\\Practical no 10\\QN3\\QN3\\App_Data\\Database1.mdf\";Integrated Security=True";
-            SqlConnection con = new SqlConnection(connectionString);
-            string insertQuery = "INSERT INTO Accountinfo (Fname, Lname, age, AccountType, Address, Email, Mobile) VALUES ('" + Fname + "', '" + Lname + "', " + age + ", '" + accounttype + "', '" + address + "', '" + email + "', " + mobile + ")";
-            SqlCommand cmd = new SqlCommand(insertQuery,con);
-            con.Open();
-            int i = cmd.ExecuteNonQuery();
-            con.Close();
-            if (i == 0)
+            try
             {
-                lblMessage.Text = "Registration Failed";
+                string Fname = TextBox1.Text;
+                string Lname = TextBox2.Text;
+                int age = int.Parse(TextBox3.Text);
+                string accounttype = DropDownList1.SelectedValue;
+                string address = TextBox6.Text;
+                string email = TextBox4.Text;
+                int mobile = Int32.Parse(TextBox5.Text);
+
+                String connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"B:\\Collage BCA\\Sybca\\Sem 4\\SYBCA-Practicals\\ASP\\Practical no 10\\QN3\\QN3\\App_Data\\Database1.mdf\";Integrated Security=True";
+                SqlConnection con = new SqlConnection(connectionString);
+                string insertQuery = "INSERT INTO Accountinfo (Fname, Lname, age, AccountType, Address, Email, Mobile) VALUES ('" + Fname + "', '" + Lname + "', " + age + ", '" + accounttype + "', '" + address + "', '" + email + "', " + mobile + ")";
+                SqlCommand cmd = new SqlCommand(insertQuery, con);
+                con.Open();
+                int i = cmd.ExecuteNonQuery();
+                con.Close();
+                if (i == 0)
+                {
+                    lblMessage.Text = "Registration Failed";
+                    lblMessage.Visible = true;
+                    lblMessage.ForeColor = System.Drawing.Color.Red;
+                }
+                else
+                {
+                    lblMessage.Text = "Registration Successful";
+                    lblMessage.Visible = true;
+                    lblMessage.ForeColor = System.Drawing.Color.Green;
+                }
+            }
+            catch (Exception ex)
+            {
+                lblMessage.Text = "Registration Failed" + ex.ToString();
                 lblMessage.Visible = true;
                 lblMessage.ForeColor = System.Drawing.Color.Red;
-            }
-            else
-            {
-                lblMessage.Text = "Registration Successful";
-                lblMessage.Visible = true;
-                lblMessage.ForeColor = System.Drawing.Color.Green;
-            }
 
+            }
         }
 
         protected void Button2_Click(object sender, EventArgs e)
@@ -55,24 +66,41 @@ namespace QN3
 
         protected void Button3_Click(object sender, EventArgs e)
         {
-            String connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"B:\\Collage BCA\\Sybca\\Sem 4\\SYBCA-Practicals\\ASP\\Practical no 10\\QN3\\QN3\\App_Data\\Database1.mdf\";Integrated Security=True";
-            String query = "delete from Accountinfo where Fname='" + TextBox1 + "'";
-            SqlCommand cmd = new SqlCommand(query, new SqlConnection(connectionString));
-            cmd.Connection.Open();
-            int i = cmd.ExecuteNonQuery();
-            if (i == 0)
+            try
             {
-                lblMessage.Text = "Deletion Failed";
+                String connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"B:\\Collage BCA\\Sybca\\Sem 4\\SYBCA-Practicals\\ASP\\Practical no 10\\QN3\\QN3\\App_Data\\Database1.mdf\";Integrated Security=True";
+                String query = "DELETE FROM Accountinfo WHERE Fname = @Fname"; // Using parameterized query
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        // Add parameter with the actual text value from TextBox1
+                        cmd.Parameters.AddWithValue("@Fname", TextBox1.Text);
+
+                        con.Open();
+                        int i = cmd.ExecuteNonQuery();
+
+                        if (i == 0)
+                        {
+                            lblMessage.Text = "Deletion Failed - No record found with that First Name";
+                            lblMessage.Visible = true;
+                            lblMessage.ForeColor = System.Drawing.Color.Red;
+                        }
+                        else
+                        {
+                            lblMessage.Text = "Deletion Successful";
+                            lblMessage.Visible = true;
+                            lblMessage.ForeColor = System.Drawing.Color.Green;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                lblMessage.Text = "Deletion Failed: " + ex.Message;
                 lblMessage.Visible = true;
                 lblMessage.ForeColor = System.Drawing.Color.Red;
             }
-            else
-            {
-                lblMessage.Text = "Deletion Successful";
-                lblMessage.Visible = true;
-                lblMessage.ForeColor = System.Drawing.Color.Green;
-            }
-            cmd.Connection.Close();
         }
     }
 }
